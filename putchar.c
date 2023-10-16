35efd7e53be350f523439dddd8867fa447c14d09
calebkech <calebkemboi61@gmail.com>
2023-10-16 20:31:42 +0300

    upload putchar.c

---
+++ putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes chacacter(s)
+ * to stdout one char at a time
+ *
+ * @c: current char
+ *
+ * Return: (1) success (-1) failure
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
